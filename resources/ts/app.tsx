dcf22e3169aff5f62185a421dc9ec4687811cf90
ruderude <rude1979@gmail.com>
2022-08-23 02:12:35 +0900

    Material-UIの導入

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core';
 import '../css/app.css';
 
 const App = () => {
   const title: string = "Laravel 9 Vite with TypeScript React !!";
-  return <h1>{title}</h1>;
+  return (<>
+    <h1>{title}</h1>
+    <Button color="secondary" variant="contained">Hello World</Button>
+    </>);
 };
  
 ReactDOM.render(
